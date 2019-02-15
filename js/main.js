550791b06da566cc408a9d3a11ecaf023f404c9d
RAJ <prajapatiraj.97@gmail.com>
2019-02-15 12:34:14 +0530

    almost final

--- js/main.js
+++ js/main.js
@@ -1,24 +1,62 @@
-document.getElementById('A').onclick = function() {
-  document.getElementById("about").style.display = "";
-  document.getElementById("projects").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("contact").style.display = "none";
- };
- document.getElementById('B').onclick = function() {
-   document.getElementById("about").style.display = "none";
-   document.getElementById("projects").style.display = "";
-   document.getElementById("skills").style.display = "none";
-   document.getElementById("contact").style.display = "none";
-};
-document.getElementById('C').onclick = function() {
-    document.getElementById("about").style.display = "none";
-    document.getElementById("projects").style.display = "none";
-    document.getElementById("skills").style.display = "";
-    document.getElementById("contact").style.display = "none";
-};
-document.getElementById('D').onclick = function() {
-  document.getElementById("about").style.display = "none";
-  document.getElementById("projects").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("contact").style.display = "";
-};
+/*var ul = document.getElementById('buttons');
+if(ul){
+  console.log(1);
+}
+ul.addEventListener('onclick', function(e) {
+    if (e.target.tagName === 'LI'){
+      if (e.target.id=='A'){
+        console.log(1111);
+        document.getElementById("about").style.display = "";
+        document.getElementById("projects").style.display = "none";
+        document.getElementById("skills").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+      }
+      if (e.target.id=='B'){
+        document.getElementById("about").style.display = "none";
+        document.getElementById("projects").style.display = "";
+        document.getElementById("skills").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+      }
+      if (e.target.id=='C'){
+        document.getElementById("about").style.display = "none";
+        document.getElementById("projects").style.display = "none";
+        document.getElementById("skills").style.display = "";
+        document.getElementById("contact").style.display = "none";
+      }
+      if (e.target.id=='D'){
+        document.getElementById("about").style.display = "none";
+        document.getElementById("projects").style.display = "none";
+        document.getElementById("skills").style.display = "none";
+        document.getElementById("contact").style.display = "";
+      }
+    }
+});*/
+var v;
+function change(v){
+    if (v==1){
+      console.log(1111);
+      document.getElementById("about").style.display = "";
+      document.getElementById("projects").style.display = "none";
+      document.getElementById("skills").style.display = "none";
+      document.getElementById("contact").style.display = "none";
+    }
+    if (v==2){
+      console.log(2222);
+      document.getElementById("about").style.display = "none";
+      document.getElementById("projects").style.display = "";
+      document.getElementById("skills").style.display = "none";
+      document.getElementById("contact").style.display = "none";
+    }
+    if (v==3){
+      document.getElementById("about").style.display = "none";
+      document.getElementById("projects").style.display = "none";
+      document.getElementById("skills").style.display = "";
+      document.getElementById("contact").style.display = "none";
+    }
+    if (v==4){
+      document.getElementById("about").style.display = "none";
+      document.getElementById("projects").style.display = "none";
+      document.getElementById("skills").style.display = "none";
+      document.getElementById("contact").style.display = "";
+    }
+}
