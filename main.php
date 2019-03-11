f8cc64d1985cfa0f3a5c37977cf93a033b400435
RAJ <prajapatiraj.97@gmail.com>
2019-03-11 19:40:57 +0530

    Form and CV

--- main.php
+++ main.php
@@ -1,15 +1,12 @@
-<html>
-<body>
 <?php
-$name = $_POST['firstname'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-if($action = 'Submit') {
-  $myfile = fopen('responses.txt', 'w') or die('Unable to open file!');
-fwrite($myfile, $firstname);
-fwrite($myfile, $email);
-fwrite($myfile, $subject);
+if(isset($_POST)){
+  $name = $_POST['firstname'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  if($action = 'Submit') {
+    $myfile = fopen('responses.txt', 'w') or die('Unable to open file!');
+  fwrite($myfile, $firstname);
+  fwrite($myfile, $email);
+  fwrite($myfile, $subject);  
 }
 ?>
-</body>
-</html>
